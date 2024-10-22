5490d7fbb5367df50cc1e4b29318b4e9429cc648
cions <gh.cions@gmail.com>
2024-10-22 23:26:45 +0900

    Add examples

---
+++ example_parses_test.go
@@ -0,0 +1,135 @@
+// Copyright (c) 2024 cions
+// Licensed under the MIT License. See LICENSE for details.
+
+package options_test
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/cions/go-options"
+)
+
+type ExampleGlobalOptions struct {
+	Config  string
+	Verbose bool
+}
+
+func (opts *ExampleGlobalOptions) Kind(name string) options.Kind {
+	switch name {
+	case "-c", "--config":
+		return options.Required
+	case "-v", "--verbose":
+		return options.Boolean
+	case "-h", "--help":
+		return options.Boolean
+	case "--version":
+		return options.Boolean
+	default:
+		return options.Unknown
+	}
+}
+
+func (opts *ExampleGlobalOptions) Option(name, value string, hasValue bool) error {
+	switch name {
+	case "-c", "--config":
+		opts.Config = value
+	case "-v", "--verbose":
+		opts.Verbose = true
+	case "-h", "--help":
+		return options.ErrHelp
+	case "--version":
+		return options.ErrVersion
+	}
+	return nil
+}
+
+type ExampleRunOptions struct {
+	ExampleGlobalOptions
+	DryRun  bool
+	Files   []string
+	Command []string
+}
+
+func (opts *ExampleRunOptions) Kind(name string) options.Kind {
+	switch name {
+	case "-n", "--dry-run":
+		return options.Boolean
+	default:
+		return opts.ExampleGlobalOptions.Kind(name)
+	}
+}
+
+func (opts *ExampleRunOptions) Option(name, value string, hasValue bool) error {
+	switch name {
+	case "-n", "--dry-run":
+		opts.DryRun = true
+	default:
+		return opts.ExampleGlobalOptions.Option(name, value, hasValue)
+	}
+	return nil
+}
+
+func (opts *ExampleRunOptions) Args(before, after []string) error {
+	if len(after) == 0 {
+		return options.ErrHelp
+	}
+	opts.Files = before
+	opts.Command = after
+	return nil
+}
+
+func ExampleParseS() {
+	opts := &ExampleGlobalOptions{
+		Config: "example.conf",
+	}
+
+	// args, err := options.ParseS(opts, os.Args[1:])
+	args, err := options.ParseS(opts, []string{"run", "-v", "file", "--", "cat"})
+	if err == nil && len(args) == 0 {
+		err = options.ErrHelp
+	}
+	if errors.Is(err, options.ErrHelp) {
+		fmt.Println("Usage: example [-c FILE] [-v] run [-n] [FILE...] -- COMMAND [ARGS...]")
+		os.Exit(0)
+	} else if errors.Is(err, options.ErrVersion) {
+		fmt.Println("example 1.0.0")
+		os.Exit(0)
+	} else if err != nil {
+		fmt.Fprintf(os.Stdout, "example: error: %v\n", err)
+		os.Exit(2)
+	}
+
+	switch args[0] {
+	case "run":
+		runopts := &ExampleRunOptions{
+			ExampleGlobalOptions: *opts,
+		}
+		_, err = options.Parse(runopts, args[1:])
+		if errors.Is(err, options.ErrHelp) {
+			fmt.Println("Usage: example [-c FILE] [-v] run [-n] [FILE...] -- COMMAND [ARGS...]")
+			os.Exit(0)
+		} else if errors.Is(err, options.ErrVersion) {
+			fmt.Println("example 1.0.0")
+			os.Exit(0)
+		} else if err != nil {
+			fmt.Fprintf(os.Stdout, "example: error: %v\n", err)
+			os.Exit(2)
+		}
+		fmt.Printf("runopts.Config: %v\n", runopts.Config)
+		fmt.Printf("runopts.Verbose: %v\n", runopts.Verbose)
+		fmt.Printf("runopts.DryRun: %v\n", runopts.DryRun)
+		fmt.Printf("runopts.Files: %v\n", runopts.Files)
+		fmt.Printf("runopts.Command: %v\n", runopts.Command)
+	default:
+		fmt.Fprintf(os.Stdout, "example: error: unknown subcommand %q. See 'example --help'.\n", args[0])
+		os.Exit(2)
+	}
+	// Output:
+	// runopts.Config: example.conf
+	// runopts.Verbose: true
+	// runopts.DryRun: false
+	// runopts.Files: [file]
+	// runopts.Command: [cat]
+}
---
+++ example_test.go
@@ -0,0 +1,116 @@
+// Copyright (c) 2024 cions
+// Licensed under the MIT License. See LICENSE for details.
+
+package options_test
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/cions/go-options"
+)
+
+type ExampleOptions struct {
+	All     bool
+	Verbose bool
+	File    *os.File
+	Number  int
+	Color   string
+}
+
+func (opts *ExampleOptions) Kind(name string) options.Kind {
+	switch name {
+	case "-a", "--all":
+		return options.Boolean
+	case "-v", "--verbose":
+		return options.Boolean
+	case "-f", "--file":
+		return options.Required
+	case "-n", "--number":
+		return options.Required
+	case "--color":
+		return options.Optional
+	case "-h", "--help":
+		return options.Boolean
+	case "--version":
+		return options.Boolean
+	default:
+		return options.Unknown
+	}
+}
+
+func (opts *ExampleOptions) Option(name, value string, hasValue bool) error {
+	switch name {
+	case "-a", "--all":
+		opts.All = true
+	case "-v", "--verbose":
+		opts.Verbose = true
+	case "-f", "--file":
+		if value == "-" {
+			opts.File = os.Stdin
+		} else {
+			fh, err := os.Open(value)
+			if err != nil {
+				return err
+			}
+			opts.File = fh
+		}
+	case "-n", "--number":
+		parsed, err := strconv.ParseInt(value, 10, strconv.IntSize)
+		if err != nil {
+			return err
+		}
+		opts.Number = int(parsed)
+	case "--color":
+		if !hasValue {
+			value = "always"
+		}
+		switch value {
+		case "always", "never", "auto":
+			opts.Color = value
+		default:
+			return errors.New("possible values are 'always', 'never', 'auto'")
+		}
+	case "-h", "--help":
+		return options.ErrHelp
+	case "--version":
+		return options.ErrVersion
+	}
+	return nil
+}
+
+func Example() {
+	opts := &ExampleOptions{
+		File:  os.Stdin,
+		Color: "auto",
+	}
+
+	// args, err := options.Parse(opts, os.Args[1:])
+	args, err := options.Parse(opts, []string{"-av", "-n", "42", "--color", "argument"})
+	if errors.Is(err, options.ErrHelp) {
+		fmt.Println("Usage: example [-av] [-f FILE] [-n NUM] [--color[={always,never,auto}]] [ARGS...]")
+		os.Exit(0)
+	} else if errors.Is(err, options.ErrVersion) {
+		fmt.Println("example 1.0.0")
+		os.Exit(0)
+	} else if err != nil {
+		fmt.Fprintf(os.Stdout, "example: error: %v\n", err)
+		os.Exit(2)
+	}
+
+	fmt.Printf("opts.All: %v\n", opts.All)
+	fmt.Printf("opts.Verbose: %v\n", opts.Verbose)
+	fmt.Printf("opts.File: %v\n", opts.File.Name())
+	fmt.Printf("opts.Number: %v\n", opts.Number)
+	fmt.Printf("opts.Color: %v\n", opts.Color)
+	fmt.Printf("Arguments: %v\n", args)
+	// Output:
+	// opts.All: true
+	// opts.Verbose: true
+	// opts.File: /dev/stdin
+	// opts.Number: 42
+	// opts.Color: always
+	// Arguments: [argument]
+}
